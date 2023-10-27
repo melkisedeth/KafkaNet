deff465c5f6854ec2907f273ae3b0cffbc9a2c05
Melky Tobias <melkytobias10@gmail.com>
2023-10-27 08:44:17 -0500

    Primer commit

---
+++ KafkaApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092"};
+
+var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? state = "";
+    while ((state = Console.ReadLine()) != null)
+    {
+        var response = await producer.ProduceAsync("weather-topic", new Message<Null, string> { Value = JsonConvert.SerializeObject(new Weather(state,70)) });
+        Console.WriteLine(response.Value);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string State, int Temperature);
